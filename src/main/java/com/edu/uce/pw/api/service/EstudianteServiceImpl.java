161d3356885ad46269b6d9ff970bb5d23a4ed71c
Diego232323A <dachicaizaq@uce.edu.ec>
2024-06-26 19:48:47 -0500

    Inicio U3

---
+++ src/main/java/com/edu/uce/pw/api/service/EstudianteServiceImpl.java
@@ -0,0 +1,38 @@
+package com.edu.uce.pw.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edu.uce.pw.api.repository.IEstudianteRepository;
+import com.edu.uce.pw.api.repository.modelo.Estudiante;
+
+@Service
+public class EstudianteServiceImpl implements IEstudianteService {
+	
+	@Autowired
+	private IEstudianteRepository estudianteRepository;
+ 
+	@Override
+	public Estudiante buscar(Integer id) {
+		// TODO Auto-generated method stub
+		return this.estudianteRepository.seleccionar(id);
+	}
+ 
+	@Override
+	public void actualizar(Estudiante estudiante) {
+		this.estudianteRepository.actualizar(estudiante);
+	}
+ 
+	@Override
+	public void borrar(Integer id) {
+		this.estudianteRepository.eliminar(id);
+	}
+ 
+	@Override
+	public void guardar(Estudiante estudiante) {
+		this.estudianteRepository.insertar(estudiante);
+	}
+
+	
+
+}
