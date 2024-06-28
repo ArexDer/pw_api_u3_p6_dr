4b50667b865992d6bde39dc135344bf795d1973d
Diego Rivas <darivas@uce.edu.ec>
2024-06-27 21:31:53 -0500

    actualizacion de clases

--- src/main/java/com/edu/uce/pw/api/service/IEstudianteService.java
+++ src/main/java/com/edu/uce/pw/api/service/IEstudianteService.java
@@ -3,17 +3,8 @@ package com.edu.uce.pw.api.service;
 import com.edu.uce.pw.api.repository.modelo.Estudiante;
 
 public interface IEstudianteService {
-	
-	//CRUD
-    public Estudiante buscar(Integer id);
-	
-	public void actualizar(Estudiante estudiante);
-	
-	public void borrar(Integer id);
-	
-	public void guardar(Estudiante estudiante);
-	
 
+    // CRUD
     public Estudiante buscar(Integer id);
 
     public void actualizar(Estudiante estudiante);
